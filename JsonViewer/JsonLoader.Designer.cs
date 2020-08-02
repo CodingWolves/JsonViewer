39aebff14c13c8dc1b97c013f820edce260c4f34
idozahavy <idozahavy@gmail.com>
2020-08-02 19:24:24 +0300

    #3 both form text changes

--- JsonViewer/JsonLoader.Designer.cs
+++ JsonViewer/JsonLoader.Designer.cs
@@ -38,9 +38,10 @@
             // 
             // LoaderButton
             // 
-            this.LoaderButton.Location = new System.Drawing.Point(124, 40);
+            this.LoaderButton.Location = new System.Drawing.Point(186, 62);
+            this.LoaderButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.LoaderButton.Name = "LoaderButton";
-            this.LoaderButton.Size = new System.Drawing.Size(135, 47);
+            this.LoaderButton.Size = new System.Drawing.Size(202, 72);
             this.LoaderButton.TabIndex = 0;
             this.LoaderButton.Text = "Load";
             this.LoaderButton.UseVisualStyleBackColor = true;
@@ -48,25 +49,28 @@
             // 
             // FilePathTextBox
             // 
-            this.FilePathTextBox.Location = new System.Drawing.Point(66, 14);
+            this.FilePathTextBox.Location = new System.Drawing.Point(99, 22);
+            this.FilePathTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.FilePathTextBox.Name = "FilePathTextBox";
-            this.FilePathTextBox.Size = new System.Drawing.Size(162, 20);
+            this.FilePathTextBox.Size = new System.Drawing.Size(241, 26);
             this.FilePathTextBox.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 17);
+            this.label1.Location = new System.Drawing.Point(18, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.Size = new System.Drawing.Size(71, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "File Path";
             // 
             // FilePathDialogButton
             // 
-            this.FilePathDialogButton.Location = new System.Drawing.Point(234, 12);
+            this.FilePathDialogButton.Location = new System.Drawing.Point(351, 18);
+            this.FilePathDialogButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.FilePathDialogButton.Name = "FilePathDialogButton";
-            this.FilePathDialogButton.Size = new System.Drawing.Size(25, 23);
+            this.FilePathDialogButton.Size = new System.Drawing.Size(38, 35);
             this.FilePathDialogButton.TabIndex = 0;
             this.FilePathDialogButton.Text = "...";
             this.FilePathDialogButton.UseVisualStyleBackColor = true;
@@ -79,16 +83,17 @@
             // 
             // JsonLoader
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(271, 99);
+            this.ClientSize = new System.Drawing.Size(406, 152);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.FilePathTextBox);
             this.Controls.Add(this.FilePathDialogButton);
             this.Controls.Add(this.LoaderButton);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "JsonLoader";
-            this.Text = "Form1";
+            this.Text = "Json File Loader";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.JsonLoader_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
--- JsonViewer/JsonView.Designer.cs
+++ JsonViewer/JsonView.Designer.cs
@@ -37,20 +37,22 @@
             this.TreeView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.TreeView.Location = new System.Drawing.Point(12, 12);
+            this.TreeView.Location = new System.Drawing.Point(18, 18);
+            this.TreeView.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.TreeView.Name = "TreeView";
-            this.TreeView.Size = new System.Drawing.Size(776, 420);
+            this.TreeView.Size = new System.Drawing.Size(1162, 644);
             this.TreeView.TabIndex = 1;
             // 
             // JsonView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 444);
+            this.ClientSize = new System.Drawing.Size(1200, 683);
             this.Controls.Add(this.TreeView);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "JsonView";
-            this.Text = "JsonView";
+            this.Text = "Json Tree View";
             this.ResumeLayout(false);
 
         }
